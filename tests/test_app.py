ddb295dd855c573d72d1ddff66f85662c119554b
K0nr45 <wrx81992@student.wroclaw.merito.pl>
2024-06-03 21:50:06 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
+import os
+import sys
 import pytest
+
+# Dodaj katalog główny projektu do ścieżki modułów
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 @pytest.fixture
@@ -6,6 +12,7 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_hello_world(client):
-    rv = client.get('/')
-    assert rv.data == b'Hello, World!'
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
